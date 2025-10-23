df7734e69571c8108194246036d45f3c2f160865
RirikaHonma <gotrhmmary@gmail.com>
2025-10-23 22:39:57 +0900

    kadai_007 をアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,14 @@
+let num = Math.ceil(Math.random() * 15)
+
+if (num % 3 == 0 && num % 5 == 0) {
+  console.log(num + ": 3と5の倍数です");
+}
+else if (num % 3 == 0) {
+  console.log(num + ": 3の倍数です");
+}
+else if (num % 5 == 0) {
+  console.log(num + ": 5の倍数です");
+}
+else {
+  console.log(num);
+}
